12a3b9b8af9a9d06f5e8409284e439371f068a95
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-11-10 10:28:53 -0600

    Adds strong params

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::MunchiesController < ApplicationController
 
   def on_arrival
+    
+    require "pry"; binding.pry
+
+
+
+  end
+
+  private
+
+  def strong_params
+    params.permit(:start, :end, :food)
 
   end
 
