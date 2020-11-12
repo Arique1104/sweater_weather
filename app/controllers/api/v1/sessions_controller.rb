6bae0cd0f2d0673ac0d049181ecdc7c524539937
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-11-11 19:17:30 -0600

    Adds session controller with create action.

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      render json: UsersSerializer.new(user), status: 200
+    else
+      render body: "Error, please try again", status: 400
+    end
+  end
+
+end
