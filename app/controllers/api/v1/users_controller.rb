0f76837c4ba4eb271e73881e39a00934b3197d28
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-11-11 18:07:17 -0600

    Updates user controller for sad path and status.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(strong_params)
     if user.save
-      render json: UsersSerializer.new(user)
+      render json: UsersSerializer.new(user), status: 201
     else
-      render body: nil, status: :no_content
+      render body: user.errors, status: 400
     end
   end
 
