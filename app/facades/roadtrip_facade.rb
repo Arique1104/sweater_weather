16de1a9dcc74db150c28cdc24a724d3f12407fdc
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-11-12 18:11:59 -0600

    Refactored Roadtrip facade.

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -1,45 +1,60 @@
 class RoadtripFacade
+
   def self.create(origin, destination)
     @origin = origin
     @destination = destination
     @distance = find_distance
-    #------------------------------------ HOW FAR
-    if @distance[:info][:statuscode] == 0
-      travel_time = @distance[:route][:formattedTime]
-      seconds = @distance[:route][:realTime]
 
-      minutes = seconds / 60.0
-      hours = ((seconds / 60) / 60)
+    impossible_route?
+    set_roadtrip
+  end
 
-      #------------------------------------ HOURLY WEATHER ARRAY FOR DESTINATION
-      forecast = find_forecast_destination
-      hours_needed = hours
-      hourly_weather = Forecast.new(forecast, hours_needed).hourly_weather
-      hourly_weather.unshift("place_holder")
-      found_forecast = hourly_weather[hours.ceil]
-
-      weather_at_eta = {
-        conditions: found_forecast.conditions,
-        temperature: found_forecast.temp.to_i
-      }
-    else
-      travel_time = 'impossible route'
-      weather_at_eta = ""
-    end
-
-    Roadtrip.new(origin, @destination, travel_time, weather_at_eta)
+  def self.set_roadtrip
+    Roadtrip.new(@origin, @destination, @travel_time, @weather_at_eta)
   end
 
   def self.find_distance
     MapService.find_destination_time(@origin, @destination)
   end
 
+  def self.impossible_route?
+    if @distance[:info][:statuscode] == 0
+      find_travel_time
+    else
+      @travel_time = 'impossible route'
+      @weather_at_eta = ""
+    end
+  end
+
+  def self.lat_long_search
+    MapService.find_long_lat(@destination)
+  end
+
+  def self.format_lat_long
+    LongLat.new(lat_long_search)
+  end
+
   def self.find_forecast_destination
+    ForecastService.find_forecast(format_lat_long)
+  end
 
-    long_lat_response = MapService.find_long_lat(@destination)
-    long_lat = LongLat.new(long_lat_response)
-    forecast = ForecastService.find_forecast(long_lat)
+  def self.find_travel_time
+    @travel_time = @distance[:route][:formattedTime]
+    seconds = @distance[:route][:realTime]
 
+    minutes = seconds / 60.0
+    hours = ((seconds / 60) / 60)
+
+    forecast = find_forecast_destination
+    hours_needed = hours
+    hourly_weather = Forecast.new(forecast, hours_needed).hourly_weather
+    hourly_weather.unshift("place_holder")
+    found_forecast = hourly_weather[hours.ceil]
+
+    @weather_at_eta = {
+      conditions: found_forecast.conditions,
+      temperature: found_forecast.temp.to_i
+    }
   end
 
 end
