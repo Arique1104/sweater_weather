ed50f439e72167a4a20d2e0500cebc5fb6521070
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-11-12 19:23:52 -0600

    MapServices spec.

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe MapService do
   context 'class methods' do
-    it 'returns data to process into longitude and latitude' do
+    it 'find_long_lat' do
       data = 'chicago,il'
       map_service = MapService.find_long_lat(data)
 
@@ -31,6 +31,30 @@ describe MapService do
 
       expect(result).to have_key(:locations)
       expect(result[:locations]).to be_a(Array)
+    end
+
+    it 'find_destination_time' do
+      origin = 'Denver,CO'
+      destination = 'Pueblo,CO'
+      map_service = MapService.find_destination_time(origin, destination)
+
+      expect(map_service).to be_a(Hash)
+      expect(map_service).to have_key(:route)
+      expect(map_service[:route]).to be_a(Hash)
+      route = map_service[:route]
+      expect(route).to be_a(Hash)
+      expect(route).to have_key(:formattedTime)
+      expect(route[:formattedTime]).to be_a(String)
+
+      expect(route).to have_key(:realTime)
+      expect(route[:realTime]).to be_a(Integer)
+
+      expect(route).to have_key(:distance)
+      expect(route[:distance]).to be_a(Integer).or be_a(Float)
+
+      expect(route).to have_key(:routeError)
+      expect(route[:routeError]).to be_an(Hash)
+
     end
   end
 end
